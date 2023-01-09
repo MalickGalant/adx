d2ec31ae0afe55c6ba68378aca00676f177e1eb0
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2023-01-09 23:41:07 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+echo "# adx" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:MalickGalant/adx.git
+git push -u origin main
